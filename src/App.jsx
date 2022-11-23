b65d1b4e8b7ab3de5ca3f9e0e64ebb2e0e2cf0aa
obinasBaba <henokgetachew500@gmail.com>
2022-11-23 15:55:41 +0300

    master- fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Count } from "./Count";
 
 function App() {
-  const [text, setText] = React.useState("");
-  const [text2, setText2] = React.useState("");
 
-  const onOdd = React.useCallback(() => setText(""), [setText]);
-  const data = React.useMemo(
-      () => ({
-        text2,
-        isEven: text2.length % 2 === 0
-      }),
-      [text2]
-  );
 
   return (
-      <div className="App">
-        <input value={text} onChange={e => setText(e.target.value)} />
-        <input
-            placeholder="text2"
-            value={text2}
-            onChange={e => setText2(e.target.value)}
-        />
-        <Count onOdd={onOdd} data={data} />
-      </div>
+      <h1>
+          go to 'first' branch
+      </h1>
   );
 }
 export default App;
