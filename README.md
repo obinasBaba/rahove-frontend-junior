58bc7c69b018c22d2894f920fb31fa5117497a22
obinasBaba <henokgetachew500@gmail.com>
2022-11-23 10:38:52 +0300

    third final

---
+++ README.md
@@ -0,0 +1,6 @@
+
+### desc : we are passing data about the `text2` state to the count component 
+
+# problem - the count component rerender when unrelated state change ( `text1` )
+
+# make the count component to only rerender when `text2` field changes
--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,23 @@ function App() {
   const [text2, setText2] = React.useState("");
 
   const onOdd = React.useCallback(() => setText(""), [setText]);
-  const data = React.useMemo(
-      () => ({
-        text2,
-        isEven: text2.length % 2 === 0
-      }),
-      [text2]
-  );
+
+  const data = {
+      text2,
+      isEven: text2.length % 2 === 0
+  }
 
   return (
       <div className="App">
         <input value={text} onChange={e => setText(e.target.value)} />
+
         <input
             placeholder="text2"
             value={text2}
             onChange={e => setText2(e.target.value)}
         />
+
+
         <Count onOdd={onOdd} data={data} />
       </div>
   );
